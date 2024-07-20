0cddf7a7ba2d50b5a1b0c20e38034fe38ab15ba5
Nwabuoku Chinedu Paul <chinedunwabuoku414@gmail.com>
2024-07-20 20:39:14 +0100

    modify

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+name='Student_Performance_Indicator',
+version='0.0.1',
+author='Nwabuoku Chinedu Paul',
+author_email='chinedunwabuoku414.com',
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn']
+
+)
